0c18b42c860ae04eb12a3bf8f40ce43d967605ca
Mwaron <135520681+Mwaron@users.noreply.github.com>
2025-11-16 12:08:07 +0100

    test

--- files/tst.py
+++ files/tst.py
@@ -2,8 +2,11 @@ import random
 import time
 import tkinter as tk
 import tkinter.ttk as ttk
+from cProfile import label
+from mimetypes import inited
 
-"""
+
+
 #Import all the texts
 f = open('Texts', 'r', encoding="utf-8")
 sz = []
@@ -36,7 +39,7 @@ def count_mistakes(given, users_input):
                 mistakes += 1
         return mistakes
 
-print("You Will get a sentence and you will have to type it as fast as you can!\n")
+""""    
 
 for line in sz:
     num = random.randint(0, len(sz))
@@ -64,21 +67,58 @@ for line in sz:
 
 
 """
+
+
 class MyGUI:
     def __init__(self):
-        self.root = tk.Tk()
 
+        self.root = tk.Tk()
+        self.root.config(bg="#fff")
         self.root.geometry("600x500")
         self.root.title("Typing Speed Test")
 
-        self.label = tk.Label(self.root, text="You Will get a sentence and you will have to type it as fast as you can!")
-        self.label.pack(padx=10, pady=10)
+        self.Name = tk.Label(self.root, font=('Arial', 40), text="Typing Speed Test")
+        self.Name.pack(padx=20, pady=50)
 
-        self.start_button = tk.Button(self.root, text="Start")
-        self.start_button.pack(padx=10, pady=10)
+        self.center_frame = tk.Frame(self.root)
+        self.center_frame.place(relx=0.5, rely=0.5, anchor='center')  # Center the frame
+
+        # Now add widgets to the frame
+        self.label = tk.Label(self.center_frame, font=('Arial', 20), text="You will get a sentence and you will have to type it as fast as you can!")
+        self.label.pack(pady=(0, 10))  # Add some space below the label
+
+        self.start_button = tk.Button(self.center_frame, text="Start", command=self.on_start_button)
+        self.start_button.pack()
 
 
         self.root.mainloop()
 
+    def on_start_button(self):
+        self.center_frame.place_forget()  # This hides the button
+
+        num = random.randint(0, len(sz))
+        Curr_line = sz[num]
+        self.start_time = time.time()
+
+        cntext = tk.Label(self.root, text=f"{Curr_line}")
+        cntext.pack(pady=10)
+
+        self.user_input = tk.Entry(self.root)
+        self.user_input.bind("<Return>", self.on_end_button)
+        self.user_input.pack(pady=10)
+
+        print("Button was clicked!")
+        #self.start_button.pack(pady=10) #I should reappear it when i finished the text...
+
+
+
+    def on_end_button(self, event):
+        self.user_input.pack_forget()
+        end_time = time.time()
+        elapsed_time = end_time - self.start_time  # Calculate the time it took to type the sentence
+        print(elapsed_time)
+
+        self.label = tk.Label(self.root, font=('Arial', 20), text=f"Nice work! Your time is: {round(elapsed_time, 2)}")
+        self.label.pack(pady=20, padx=20)
 
 MyGUI()
